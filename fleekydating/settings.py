edbeb12eebbe8bfab2b76ae05606b5d2238e7dc7
Ronan Pierce Higgins <ronan.higgins@ucdconnect.ie>
2016-01-06 22:55:23 +0000

    Update settings.py

--- fleekydating/settings.py
+++ fleekydating/settings.py
@@ -8,6 +8,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'ronanhiggins8@gmail.com'
+EMAIL_HOST_PASSWORD = 'xuxrboueungkgbll'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
 SITE_ID = 1
 
 AUTH_USER_MODEL = 'accounts.User'
